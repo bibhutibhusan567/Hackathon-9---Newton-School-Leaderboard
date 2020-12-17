e8f3a4d250f1d010ef650ae0db47a000e07b484b
bibhutibhusan567 <bibhutibhusan567@gmail.com>
2020-12-17 17:46:43 +0530

    done

--- src/index.js
+++ src/index.js
@@ -13,6 +13,20 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 // your code goes here
 
+const getProperVal = (value, defaultValue) =>
+    (value === null || value === undefined || isNaN(value)) ? defaultValue : Number(value);
+
+app.get('/topRankings', async (req, res) => {
+    const offset = getProperVal(req.query.offset, 0);
+    const limit = getProperVal(req.query.limit, 20) + offset;
+    console.log(`limit:${limit} offset:${offset}`);
+
+    const response = await data.slice(offset, limit);
+
+    res.send(response);
+
+});
+
 
 app.listen(port, () => console.log(`App listening on port ${port}!`))
 
